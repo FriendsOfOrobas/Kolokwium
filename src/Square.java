999a099ac93e7c1f22747e8cef230ca1046d9436
orobas <szymon.misiewicz@interia.pl>
2023-01-16 14:47:26 +0100

    done

--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,52 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.awt.*;
 public class Square {
+    private int x;
+    private int y;
+    private int size = 10;
+    private int dy;
+    private int dx;
+    private Random random = new Random();
+    private Thread thread;
+    private Panel panel;
+    public Square(Panel panel){
+        this.panel = panel;
+        this.x = random.nextInt(60)*10;
+        this.y = 0;
+        this.dy = 10;
+        this.dx = 0;
+        thread = new Thread(() -> {
+            while (!Thread.interrupted()) {
+                panel.repaint();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
+        });
+        thread.start();
+    }
+
+    public void checkerCollision(){
+        ArrayList<Square> sqs = (ArrayList<Square>) panel.getSquares().clone();
+        sqs.remove(this);
+        for(Square sq : sqs){
+            if(y+10 == sq.y || y+10==600){
+                dy=0;
+                panel.addSquare();
+                thread.stop();
+            }
+        }
+    }
+    public void update() {
+        checkerCollision();
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+    public void draw(Graphics g) {
+        g.setColor(Color.ORANGE);
+        g.fillRect(x, y, size, size);
+    }
 }
--- src/Tetris.java
+++ src/Tetris.java
@@ -1,3 +1,27 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+public class Tetris {
 
+        private JFrame frame;
+        private Panel panel;
+        private Timer timer;
+        public Tetris() {
+            timer = new Timer(50, e -> panel.update());
+            frame = new JFrame("Tetris");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(600, 600);
+            panel = new Panel();
+            frame.add(panel);
+            frame.setVisible(true);
+        }
+
+        public void start(){
+            timer.start();
+        }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Tetris tetris = new Tetris();
+            tetris.start();
+        });
+    }
 }
